61243ecb6deb82a7784b703c26e1dc0394fb7f15
ojeda0 <abrahamojeda09@gmail.com>
2025-10-23 14:44:51 +0200

    ejercicio entrega

---
+++ ExplorandoEclipse/src/entornos/Principal.java
@@ -0,0 +1,12 @@
+package entornos;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Bienvenido a Eclipse");
+		System.out.println("Vamos a conocer el entorno de desarrollo");
+		
+	}
+
+}
