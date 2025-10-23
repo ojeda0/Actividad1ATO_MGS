7d2fd468e0c78c937e18c8004df48a418a0af0d6
ojeda0 <abrahamojeda09@gmail.com>
2025-10-23 18:55:53 +0200

    ejercicios en casa

--- ExplorandoEclipse/src/entornos/Principal.java
+++ ExplorandoEclipse/src/entornos/Principal.java
@@ -4,6 +4,13 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		
+		
+		/*
+		 * CAMBIOS REALIZADOS:
+		 * HE CAMBIADO EL TEMA DE DARK A LIGHT.
+		 * HE CAMBIADO LA FUENTE DE CONSOLA A FIXEDSYS.
+		 */
+		
 		System.out.println("Bienvenido a Eclipse");
 		System.out.println("Vamos a conocer el entorno de desarrollo");
 		
