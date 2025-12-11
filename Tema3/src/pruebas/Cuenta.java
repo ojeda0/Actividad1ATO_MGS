203c94cf8082c6836fe7af7d8f74e635ab08363e
ojeda0 <abrahamojeda09@gmail.com>
2025-12-11 14:42:58 +0100

    clase de pruebas automaticas

---
+++ Tema3/src/pruebas/Cuenta.java
@@ -0,0 +1,46 @@
+package pruebas;
+
+public class Cuenta {
+
+	//ATRIBUTOS
+	
+	private String numero;
+	private double saldo;
+	
+	public Cuenta(String numero, double saldo) {
+		this.setNumero(numero);
+		this.setSaldo(saldo);
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void ingresarDinero(double importe) {
+		setSaldo(getSaldo() + importe);
+	}
+	
+	public void extraerDinero(double importe) {
+		setSaldo(getSaldo() - importe);
+	}
+	
+	public void mostrarCuenta() {
+		
+		System.out.println("Nº Cuenta: "+getNumero());
+		System.out.println("Saldo: "+getSaldo()+ " €");
+		
+	}
+	
+}
---
+++ Tema3/src/pruebas/CuentaTest.java
@@ -0,0 +1,24 @@
+package pruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	@Test
+	void testIngresarDinero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExtraerDinero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMostrarCuenta() {
+		fail("Not yet implemented");
+	}
+
+}
